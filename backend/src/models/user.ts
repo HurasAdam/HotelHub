e625270857424615590c70acebc763119c06287c
HurasAdam <huradam@wp.pl>
2024-01-02 09:32:02 +0100

    Add User model schema.

---
+++ backend/src/models/user.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+export type UserType= {
+    _id:string;
+    email:string;
+    password:string;
+    firstName:string;
+    lastName:string;
+};
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+    },
+    firstName:{
+        type:String,
+        required:[true,'FirstName is required'],
+        lowercase:true
+    },
+    lastName:{
+        type:String,
+        required:[true,"LastName is required"],
+        lowercase:true
+    }
+})
+
+userSchema.pre("save",async function(next){
+   if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password,8)
+   }
+   next()
+})
+
+const User = mongoose.model<UserType>("User",userSchema)
+export default User
