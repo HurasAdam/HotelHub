a5f4e5d2fd20ae67f7109a3f9c7f69e6b37fb0d3
HurasAdam <huradam@wp.pl>
2024-01-02 10:23:41 +0100

    feat: Add login route and authentication logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,14 @@ import "dotenv/config";
 import mongoose, { mongo } from "mongoose";
 import {PORT,DB} from "./db/config/config";
 import userRoutes from "./routes/users";
-
+import authRoutes from "./routes/auth"
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // ------ROUTES-----------//
+app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 
 // -----DB CONNECTION-----//
