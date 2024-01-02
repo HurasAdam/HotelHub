902e80c6d569924d5f7444d39fe4a486e941d538
HurasAdam <huradam@wp.pl>
2024-01-02 09:36:12 +0100

    Refator index.ts:Add user routes and reorganize code structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,21 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose, { mongo } from "mongoose";
 import {PORT,DB} from "./db/config/config";
-
-
-
+import userRoutes from "./routes/users";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// ------ROUTES-----------//
+app.use("/api/users",userRoutes)
+
+// -----DB CONNECTION-----//
 mongoose.connect(DB as string).then(() => {
   const server = app.listen(PORT, () => {
     console.log(`Server listen on port:${PORT}`);
   });
 });
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.status(200).json("DZIALA");
-});
