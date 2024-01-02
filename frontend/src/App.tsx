03f9a370dfeccf36cf1dfbdaac9ae9164dcbdc3d
HurasAdam <huradam@wp.pl>
2024-01-02 06:05:39 +0100

    feat: Add Footer component and fix app routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,41 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import Layout from "./layouts/Layout"
-
+import Layout from "./layouts/Layout";
 
 function App() {
-
-
   return (
- <Router>
-
-  <Routes>
-    <Route path="/" element={<Layout></Layout>}/>
-    <Route/>
-    <Route/>
-  </Routes>
- </Router>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <p>Sign In</p>
+            </Layout>
+          }
+        />
+        <Route />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
