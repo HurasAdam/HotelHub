bf62fa018ad8cfb5c90797bdb03c0fc97a12cca0
HurasAdam <huradam@wp.pl>
2024-01-03 17:57:48 +0100

    feat: add withCredentials option to axios configuration

--- frontend/src/features/axios/axios.ts
+++ frontend/src/features/axios/axios.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const API_BASE_URL= import.meta.env.VITE_API_BASE_URL;
 
 const clientApi= axios.create({
-    baseURL:API_BASE_URL
+    baseURL:API_BASE_URL,
+    withCredentials:true
 })
 
 
