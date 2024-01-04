4659cfab799b1a24417afb3ecf43055577dc8150
HurasAdam <huradam@wp.pl>
2024-01-04 08:25:44 +0100

    feat: Add signIn and validateToken functions in api-client.

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,10 +1,24 @@
 import { RegisterFormData } from "./pages/Register";
 import clientApi from "./features/axios/axios";
+import { SignInFormData } from "./pages/SignIn";
+
+export const register = async (formData: RegisterFormData) => {
+  const response = await clientApi.post("users/register", formData);
+
+  return response.data;
+};
 
 
+export const signIn= async(formData:SignInFormData)=>{
+  const response = await clientApi.post("auth/login",formData);
+  return response.data;
+}
 
-export const register = (async(formData:RegisterFormData)=>{
-const response = await clientApi.post("users/register",formData)
 
-return response.data
-})
+export const validateToken = async () => {
+  const response = await clientApi.get("auth/validate-token", {});
+
+  return response.data;
+};
+
+
