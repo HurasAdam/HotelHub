5a757f9a88dcdad22bfffd213688c16389eaed58
HurasAdam <huradam@wp.pl>
2024-01-04 08:12:21 +0100

    Refactor: nrdefine ToastProps type for better readability and TypeScript support.

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -1,36 +1,34 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-type Toast={
-    message:string,
-    type:"SUCCESS" | "ERROR"
-    onClose:()=>void;
-}
+type ToastProps = {
+  message: string;
+  type: "SUCCESS" | "ERROR";
+  onClose: () => void;
+};
 
+const Toast = ({ message, type, onClose }:ToastProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 5000);
 
-const Toast = ({message,type,onClose})=>{
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onClose]);
 
-useEffect(()=>{
+  const styles =
+    type === "SUCCESS"
+      ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
+      : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
 
-    const timer = setTimeout(()=>{
-onClose()
-    },5000);
+  return (
+    <div className={styles}>
+      <div className="flex justify-center items-center">
+        <span className="text-lg font-semibold">{message}</span>
+      </div>
+    </div>
+  );
+};
 
-    return ()=>{
-        clearTimeout(timer)
-    }
-},[onClose])
-
-const styles=type==="SUCCESS"?"fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md":"fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md"
-
-
-    return(
-        <div className={styles}>
-            <div className="flex justify-center items-center">
-                <span className="text-lg font-semibold">{message}</span>
-            </div>
-        </div>
-    )
-}
-
-
-export default Toast
+export default Toast;
