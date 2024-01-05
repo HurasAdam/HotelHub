c7f97c85beddb5446c9d602fec49ac86b10051e1
HurasAdam <huradam@wp.pl>
2024-01-05 05:26:37 +0100

    Add auth e2e tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,28 +2,31 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose, { mongo } from "mongoose";
-import {PORT,DB} from "./db/config/config";
+import path from "path";
 import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth"
-import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:process.env.FRONTEND_URL,
-  credentials:true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname,"../../frontend/dist")))
 
 // ------ROUTES-----------//
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // -----DB CONNECTION-----//
-mongoose.connect(DB as string).then(() => {
-  const server = app.listen(PORT, () => {
-    console.log(`Server listen on port:${PORT}`);
+mongoose.connect(process.env.MONGO_URI as string).then(() => {
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`Server listen on port:${process.env.PORT}`);
   });
 });
-
