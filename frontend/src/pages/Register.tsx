4e3d5a405311b698b50aa6a0f3cd3bf3ac62c1d5
HurasAdam <huradam@wp.pl>
2024-01-05 05:24:00 +0100

    feat: Add useQueryClient, to invalidate aut_token and update onSuccess after register.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -21,12 +21,15 @@ const Register = () => {
     formState: { errors },
   } = useForm<RegisterFormData>();
 
+const queryClient=useQueryClient()
+
   const navigate = useNavigate();
   const { showToast } = useAppContext();
 
   const mutation = useMutation(apiClient.register, {
-    onSuccess: ({message} ) => {
+    onSuccess: async({message} ) => {
       showToast({ message: message, type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken")
       navigate("/");
     },
     onError: (error: AxiosError| Error) => {
