3eb2da07c586d47f453a11aa02d8fbdfb348a6fe
HurasAdam <huradam@wp.pl>
2024-01-06 04:42:51 +0100

    feat: Add addHotel api function to api-client.

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -4,7 +4,6 @@ import { SignInFormData } from "./pages/SignIn";
 
 export const register = async (formData: RegisterFormData) => {
   const response = await clientApi.post("users/register", formData);
-
   return response.data;
 };
 
@@ -15,13 +14,15 @@ export const signIn = async (formData: SignInFormData) => {
 
 export const validateToken = async () => {
   const response = await clientApi.get("auth/validate-token", {});
-
   return response.data;
 };
 
-
 export const signOut = async () => {
   const response = await clientApi.post("/auth/logout");
-console.log("URRRRA")
   return response.data;
+};
+
+export const addHotel = async (formData: FormData) => {
+  const response = await clientApi.post("/my-hotels", formData);
+  return response;
 };
