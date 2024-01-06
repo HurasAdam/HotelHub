afa3f4ec7716b162f3733df27f99be73c00b8e1f
HurasAdam <huradam@wp.pl>
2024-01-06 04:47:33 +0100

    feat: Add form submission and mutation in AddHotel component.

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,9 +1,31 @@
-import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm"
+import { useMutation } from "react-query";
+import ManageHotelForm, {
+  HotelFormData,
+} from "../forms/ManageHotelForm/ManageHotelForm";
+import * as clientApi from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import axios, { AxiosError } from "axios";
+const AddHotel = () => {
+  const { showToast } = useAppContext();
 
-const AddHotel = ()=>{
-    return(
-       <ManageHotelForm/>
-    )
-}
+  const { mutate, isLoading } = useMutation(clientApi.addHotel, {
+    onSuccess: () => {
+      showToast({ message: "Hotel has been added", type: "SUCCESS" });
+    },
+    onError: (error: AxiosError | Error) => {
+      if (axios.isAxiosError(error)) {
+        showToast({ message: "Error saving hotel ", type: "ERROR" });
+      } else {
+        showToast({ message: error.message, type: "ERROR" });
+      }
+    },
+  });
 
-export default AddHotel
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
+};
+
+export default AddHotel;
