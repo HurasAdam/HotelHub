2aefbd05a70baf1fd57fe704421b9abf8f20bf94
HurasAdam <huradam@wp.pl>
2024-01-06 10:35:31 +0100

    feat: Add file filter on uploading files to cloudinary.

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,59 +1,106 @@
 import express, { Request, Response } from "express";
-import multer from "multer"
-import cloudinary from "cloudinary"
+import multer,{FileFilterCallback} from "multer";
+import  cloudinary from "cloudinary"
 import { HotelType } from "../models/hotel";
-import Hotel from "../models/hotel"
+import Hotel from "../models/hotel";
 import verifyToken from "../middleware/auth";
 import { body } from "express-validator";
 const router = express.Router();
 
-const storage = multer.memoryStorage()
-const upload=multer({
-    storage:storage,
-    limits:{
-        fileSize:5*1024*1024 //5MB
-    }
-})
 
+const storage = multer.memoryStorage();
 
-router.post("/",verifyToken,[
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  const allowedMimeTypes = [
+    "jpeg",
+    "jpg",
+    "png",
+    "pdf",
+    "doc",
+    "docx",
+    "xls",
+    "octet-stream",
+  ];
+
+  if (allowedMimeTypes.includes(file.mimetype.split("/")[1])) {
+    cb(null, true);
+  } else {
+    const error = new Error(`LIMIT_UNEXPECTED_FILE${file.fieldname}`) as any;
+    cb(error, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+router.post(
+  "/",
+  verifyToken,
+  [
     body("name").notEmpty().withMessage("Name is required"),
     body("city").notEmpty().withMessage("City is required"),
     body("country").notEmpty().withMessage("Country is required"),
     body("description").notEmpty().withMessage("description is required"),
     body("type").notEmpty().withMessage("Hotel type is required"),
-    body("pricePerNight").notEmpty().isNumeric().withMessage("Price per night is required"),
-    body("adultCount").notEmpty().isNumeric().withMessage("Adult count is required"),
-    body("childCount").notEmpty().isNumeric().withMessage("Child count is required"),
-    body("facilities").notEmpty().isArray().withMessage("Facilities are required"),
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required"),
+    body("adultCount")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Adult count is required"),
+    body("childCount")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Child count is required"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
+  ],
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
 
-],upload.array("imageFiles",6), async (req: Request, res: Response) => {
-try{
-const imageFiles= req.files as Express.Multer.File[]
-const newHotel:HotelType = req.body
-const uploadPromises= imageFiles.map(async(image)=>{
-    const b64=Buffer.from(image.buffer).toString("base64")
-    let dataURI= "data:" + image.mimetype+"'base64,"+b64
-    const response = await cloudinary.v2.uploader.upload(dataURI);
-    return response.url
-})
+console.log(imageFiles)
 
-const imageUrls = await Promise.all(uploadPromises);
+      const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const response = await cloudinary.v2.uploader.upload(dataURI);
+        return response.url;
+      });
 
-newHotel.imageUrls= imageUrls
-newHotel.lastUpdated = new Date();
-newHotel.userId = req.userId
+      const imageUrls = await Promise.all(uploadPromises);
 
-const hotel = new Hotel(newHotel)
-await hotel.save()
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
 
-return res.status(201).json(hotel)
-}
-catch(error){
-console.log("Error creating hotel:",error);
-return res.status(500).json({message:"Something went wrong.Please check your input and try again. If the problem persists, contact support and provide error code"})
-}
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
 
-});
+      return res.status(201).json(hotel);
+    } catch (error) {
+      console.log("Error creating hotel:", error);
+      return res.status(500).json({
+        message:
+          "Something went wrong.Please check your input and try again. If the problem persists, contact support and provide error code",
+      });
+    }
+  }
+);
 
-export default router
+export default router;
