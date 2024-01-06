5a781a7cae58128450ea1dba4a8814d2fd74f44f
HurasAdam <huradam@wp.pl>
2024-01-06 04:44:42 +0100

    feat: Add Props type for the ManageHotelForm component, Add logic to handle form submission, Add Save button and loading state handling.

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -18,18 +18,70 @@ export type HotelFormData = {
   childCount: number;
 };
 
-const ManageHotelForm = () => {
+type Props = {
+  onSave: (hotelFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
   const formMethods = useForm<HotelFormData>();
 
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+    const {
+      name,
+      city,
+      country,
+      description,
+      type,
+      pricePerNight,
+      starRating,
+      adultCount,
+      childCount,
+      facilities,
+      imageFiles,
+    } = formDataJson;
+
+    const formData = new FormData();
+
+    formData.append("name", name);
+    formData.append("city", city);
+    formData.append("country", country);
+    formData.append("description", description);
+    formData.append("type", type);
+    formData.append("pricePerNight", pricePerNight.toString());
+    formData.append("starRating", starRating.toString());
+    formData.append("adultCount", adultCount.toString());
+    formData.append("childCount", childCount.toString());
+
+    facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]}`, facility);
+    });
+
+    Array.from(imageFiles).forEach((imageFile) => {
+      formData.append("imageFiles", imageFile);
+    });
+
+    onSave(formData);
+  });
   return (
     <FormProvider {...formMethods}>
-      <form className="flex flex-col gap-10">
-
-        <DetailsSection/>
-        <TypeSection/>
-        <FacilitiesSection/>
-        <GuestSection/>
-        <ImagesSection/>
+      <form className="flex flex-col gap-10" onSubmit={onSubmit}>
+        <DetailsSection />
+        <TypeSection />
+        <FacilitiesSection />
+        <GuestSection />
+        <ImagesSection />
+        <span className="flex justify-end">
+          <button
+            disabled={isLoading}
+            className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500"
+            type="submit"
+          >
+            {isLoading ? "Saving..." : "Save"}
+          </button>
+        </span>
       </form>
     </FormProvider>
   );
