b590095876acffeb73ad00f76e05c410fbeedbdb
HurasAdam <huradam@wp.pl>
2024-02-28 02:15:20 +0100

    Refactor image uploading logic into separate function, feat: Added update hotel endpoint.

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -74,16 +74,9 @@ router.post(
       const imageFiles = req.files as Express.Multer.File[];
       const newHotel: HotelType = req.body;
 
-      console.log(imageFiles)
 
-      const uploadPromises = imageFiles.map(async (image) => {
-        const b64 = Buffer.from(image.buffer).toString("base64");
-        let dataURI = "data:" + image.mimetype + ";base64," + b64;
-        const response = await cloudinary.v2.uploader.upload(dataURI);
-        return response.url;
-      });
 
-      const imageUrls = await Promise.all(uploadPromises);
+      const imageUrls = await uploadImages(imageFiles);
 
       newHotel.imageUrls = imageUrls;
       newHotel.lastUpdated = new Date();
@@ -129,5 +122,42 @@ router.get("/:id", verifyToken, async (req: Request, res: Response) => {
   }
 })
 
+router.put("/:hotelId", verifyToken, upload.array("imageFiles"), async (req: Request, res: Response) => {
+
+  const { hotelId } = req.params
+  const { userId } = req
+  try {
+    const updatedHotel: HotelType = req.body;
+    updatedHotel.lastUpdated = new Date();
+
+    const hotel = await Hotel.findOneAndUpdate({ _id: hotelId, userId }, updatedHotel, { new: true })
+
+    if (!hotel) {
+      return res.status(404).json({ message: "Hotel not found" });
+    }
+
+    const files = req.files as Express.Multer.File[];
+    const updatedImageUrls = await uploadImages(files);
+    hotel.imageUrls = [...updatedImageUrls, ...(updatedHotel.imageUrls || [])];
+    await hotel.save();
+
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
+
+
+
+const uploadImages = async (imageFiles: Express.Multer.File[]) => {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const response = await cloudinary.v2.uploader.upload(dataURI);
+    return response.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
 
 export default router;
