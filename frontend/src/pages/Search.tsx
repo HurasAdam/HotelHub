c41c102629dbdf1c7969a277c1733a05e0a27df1
HurasAdam <huradam@wp.pl>
2024-03-02 23:21:26 +0100

    Feat: Added StarRating component.

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -4,10 +4,12 @@ import { useQuery } from 'react-query';
 import * as apiClient from "../api-client"
 import SearchResultCard from '../components/SearchResultCard';
 import Pagination from '../components/Pagination';
+import StarRatingFilter from '../components/StarRatingFilter';
 
 const Search:React.FC = () => {
 const search= useSearchContext();
 const [page,setPage]=useState<number>(1);
+const [selectedStars,setSelectedStars]=useState<string[]>([]);
 
 const searchParams = {
   destination:search.destination,
@@ -15,12 +17,21 @@ const searchParams = {
   checkOut:search.checkOut.toISOString(),
   adultCount:search.adultCount.toString(),
   childCount:search.childCount.toString(),
-  page:page.toString()
+  page:page.toString(),
+  stars:selectedStars,
 }
 
 const {data:hotelData}=useQuery(
   ["searchHotels",searchParams], 
   ()=> apiClient.searchHotels(searchParams))
+
+
+  const handleStarChange= (event:React.ChangeEvent<HTMLInputElement>) =>{
+const starRating = event.target.value
+setSelectedStars((prevStars)=>
+event.target.checked?[... prevStars,starRating]:prevStars.filter((star)=>star!==starRating)
+);
+  }
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
@@ -29,7 +40,10 @@ const {data:hotelData}=useQuery(
 
 <div className='space-y-5'>
   <h3 className='text-lg font-semibold border-b border-slate-300 pb-5'>Filter by:</h3>
-{/* #TODO FILTERS */}
+
+<StarRatingFilter onChange={handleStarChange} selectedStars={selectedStars}  />
+
+
 </div>
 </div>
 
